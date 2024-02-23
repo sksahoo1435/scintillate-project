fd3c97f8fe32b96426170649446f2cbb6d5a8b5f
sunil kumar sahoo <sk7997717@gmail.com>
2024-02-23 10:04:10 +0530

    adding images and change the box designs

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Button, HStack, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableContainer,
-  Heading, Center, Box, Checkbox, Spinner, Avatar, Stack, Text, Tooltip
+  Button, HStack, Box, Checkbox, Spinner, Avatar, Stack, Text, Tooltip, SimpleGrid, Center
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import './main.css'
@@ -59,12 +58,21 @@ const CharacterList = () => {
 
         {/* Marquee Heading */}
         <Center h='50px'>
-          <Heading as='h3' size='md' textShadow='1.2px 1px gray' display="inline-block" animation="marquee 8s linear infinite" color='white'>
+          <Text
+            as='h3'
+            fontSize='xl'
+            fontWeight='bold'
+            textShadow='1.2px 1px gray'
+            display='inline-block'
+            animation='marquee 8s linear infinite'
+            color='white'
+          >
             CHARACTER TABLE
-          </Heading>
+          </Text>
         </Center>
 
-        <Box p={5}>
+        <Box p={5} pt='5%'>
+
           <Center>
             {/* Tooltip and Table Container */}
             <Box position="absolute" left="0" bottom="65%">
@@ -79,99 +87,102 @@ const CharacterList = () => {
               </Text>
             </Box>
 
-            {/* Character Table */}
-            <TableContainer border='1.5px' borderColor='#00b6ffba' borderRadius='1rem' boxShadow='dark-lg' rounded='md' bg='white' w='60%' h='88dvh'>
-              <Table variant="striped" size="md" colorScheme="purple">
+            <Box bgColor='white' position='relative' h='70dvh' minH='60dvh' borderRadius='md'
+              boxShadow='dark-lg'>
 
-                {/* Table Header */}
-                <Thead>
-                  <Tr h={50}>
-                    <Th textShadow='1.2px 1px gray'>Character Name</Th>
-                    <Th textShadow='1.2px 1px gray'>Favourite</Th>
-                  </Tr>
-                </Thead>
+              {/* Character List */}
+              {loadings ? (
+                <Center p={250} minW='65dvw'>
+                  <Spinner size='xl' />
+                </Center>
 
-                {/* Table Body */}
-                <Tbody style={{ overflowY: 'auto', maxHeight: '60vh' }}>
-                  {loadings ?
-                    <Center p={240}>
-                      <Spinner size='xl' />
-                    </Center> :
-                    <>
-                      {/* Map through characters to display in the table */}
-                      {characters.map((character, ind) => (
-                        <Tooltip label={`Click the ${character.name} to see the profile`} hasArrow>
-                          <Tr key={character.name} _hover={{ fontWeight: 'semibold' }}>
-                            <Td p={2}>
-                              {/* Avatar and Character Name */}
-                              <Stack direction={['column', 'row']} spacing='24px'>
-                                {ind % 2 === 0 && ind % 3 === 0
-                                  ? <Avatar name={character.name} size='sm' />
-                                  : <Avatar size='sm' src={`${ind % 2 === 0
+              ) : (
+                <SimpleGrid columns={[2, 3, 5]} spacing={4} padding={15}>
+                  {characters.map((character,ind) => (
+                    <Tooltip key={character.name} label={`Click the ${character.name} to see the profile`} hasArrow>
+                      <Box
+                        bg='white'
+                        p={4}
+                        borderRadius='md'
+                        boxShadow='dark-lg'
+                        textAlign='center'
+                      >
+                        {/* Avatar and Character Name */}
+                        <Stack direction='column' spacing='24px'>
+                          <Center>
+                            
+                            {ind % 2 === 0 && ind % 3 === 0
+                                  ? <Avatar name={character.name} size='lg' />
+                                  : <Avatar size='lg' src={`${ind % 2 === 0
                                     ? 'https://bit.ly/dan-abramov'
                                     : ind % 3 === 0
                                       ? 'https://bit.ly/sage-adebayo'
                                       : 'https://bit.ly/ryan-florence'}`} />
                                 }
-                                <Text pt={1} _hover={{ textShadow: '1px 1px gray' }}>
-                                  {/* Link to character details page */}
-                                  <Link to={`/characters/${character.url.split('/').slice(-2, -1)[0]}`}>
-                                    {character.name}
-                                  </Link>
-                                </Text>
-                              </Stack>
-                            </Td>
+                          </Center>
+                          <Text fontWeight='bold' pt={2} _hover={{ textShadow: '1px 1px gray' }}>
+                            {/* Link to character details page */}
+                            <Link to={`/characters/${character.url.split('/').slice(-2, -1)[0]}`}>
+                              {character.name}
+                            </Link>
+                          </Text>
 
-                            {/* Checkbox for marking/unmarking as favorite */}
-                            <Td>
-                              <Checkbox
-                                colorScheme='orange'
-                                borderColor="#f87501ba"
-                                isChecked={favorites.some((fav) => fav.name === character.name)}
-                                onChange={() => handleCheckboxChange(character)}
-                                _hover={{ transform: 'scale(1.05)', transition: '0.3s', boxShadow: 'dark-lg' }}
-                              />
-                            </Td>
-                          </Tr>
-                        </Tooltip>
-                      ))}
-                    </>
-                  }
-                </Tbody>
+                          {/* Checkbox for marking/unmarking as favorite */}
+                          <Checkbox
+                            colorScheme='orange'
+                            borderColor='#f87501ba'
+                            isChecked={favorites.some((fav) => fav.name === character.name)}
+                            onChange={() => handleCheckboxChange(character)}
+                            _hover={{ transform: 'scale(1.05)', transition: '0.3s', boxShadow: 'dark-lg' }}
+                          >
+                            Add to Favorites
+                          </Checkbox>
+                        </Stack>
+                      </Box>
+                    </Tooltip>
+                  ))}
+                </SimpleGrid>
+              )}
 
-                {/* Table Footer with Pagination */}
-                <Tfoot position='relative'>
-                  <Center p={2}>
-                    <HStack mt={2} spacing={2} ml={20}>
-                      {/* Previous Page Button */}
-                      <Button onClick={() => handlePageChange(page - 1)} isDisabled={page === 1} border='1px' borderColor='#f87501ba' bgGradient={page === 1 ? 'gray.200' : 'linear(to-r, #f2c996cf, #9f5019d4)'}>
-                        Previous
-                      </Button>
+              {/* Pagination at the bottom */}
+              {loadings ? "" : <HStack mt={4} spacing={2} justify='center' position='relative' bottom='2%' >
+                {/* Previous Page Button */}
+                <Button
+                  onClick={() => handlePageChange(page - 1)}
+                  isDisabled={page === 1}
+                  border='1px'
+                  borderColor='#f87501ba'
+                  bgGradient={page === 1 ? 'gray.200' : 'linear(to-r, #f2c996cf, #9f5019d4)'}
+                >
+                  Previous
+                </Button>
 
-                      {/* Page Buttons */}
-                      {Array.from({ length: totalPages }, (_, index) => (
-                        <Button
-                          key={index}
-                          onClick={() => handlePageChange(index + 1)}
-                          colorScheme={index + 1 === page ? 'orange' : 'gray'}
-                          variant={index + 1 === page ? 'solid' : 'outline'}
-                          borderColor='#f87501ba'
-                        >
-                          {index + 1}
-                        </Button>
-                      ))}
+                {/* Page Buttons */}
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <Button
+                    key={index}
+                    onClick={() => handlePageChange(index + 1)}
+                    colorScheme={index + 1 === page ? 'orange' : 'gray'}
+                    variant={index + 1 === page ? 'solid' : 'outline'}
+                    borderColor='#f87501ba'
+                  >
+                    {index + 1}
+                  </Button>
+                ))}
 
-                      {/* Next Page Button */}
-                      <Button onClick={() => handlePageChange(page + 1)} isDisabled={page === totalPages} border='1px' borderColor='#f87501ba' bgGradient={page === totalPages ? 'gray.200' : 'linear(to-r, #f2c996cf, #9f5019d4)'}>
-                        Next
-                      </Button>
-                    </HStack>
-                  </Center>
-                </Tfoot>
-              </Table>
-            </TableContainer>
+                {/* Next Page Button */}
+                <Button
+                  onClick={() => handlePageChange(page + 1)}
+                  isDisabled={page === totalPages}
+                  border='1px'
+                  borderColor='#f87501ba'
+                  bgGradient={page === totalPages ? 'gray.200' : 'linear(to-r, #f2c996cf, #9f5019d4)'}
+                >
+                  Next
+                </Button>
+              </HStack>}
 
-            {/* Tooltip for additional information */}
+            </Box>
             <Box position="absolute" right="0" bottom="50%" top="10">
               <Text
                 className="scroll-rotate-animation-bottom"
